b7018d6e88acf0b38006cc715211fc12ffe7d773
Santiago Garcia <santigarcor@gmail.com>
2026-01-20 09:48:20 -0500

    Add list of datarefs/commands to be added for the FF757

--- src/mappings/ff_757.ts
+++ src/mappings/ff_757.ts
@@ -26,15 +26,87 @@ export function initializeMappings(bridge: XPlaneArduinoBridge): void {
 }
 
 // B757
+
+// ALTITUDE:
 // Dataref:
 // 1-sim/AP/altSetting
-// 1-sim/AP/dig3/hdgSetting
-
+// 1-sim/AP/altHoldButton
 // Commands
 // 1-sim/comm/AP/altDN
 // 1-sim/comm/AP/altUP
 
+// HEADING:
+// Dataref:
+// 1-sim/AP/dig3/hdgSetting
+// 1-sim/AP/hdgHoldButton
+// Commands
 // 1-sim/comm/AP/hdgDN
 // 1-sim/comm/AP/hdgUP
 
-1 - sim / AP / hdgHoldButton
+// APP
+// Dataref:
+// 1-sim/AP/appButton
+
+// V/S
+// Dataref:
+// 1-sim/AP/vviButton
+// 1-sim/AP/vviSetting
+// Commands
+// 1-sim/comm/AP/vviUP
+// 1-sim/comm/AP/vviDN
+
+// CMD
+// Dataref:
+// 1-sim/AP/cmd_L_Button
+
+// LNAV
+// Dataref:
+// 1-sim/AP/lnavButton
+
+// VNAV
+// Dataref:
+// 1-sim/AP/vnavButton
+
+// SPD
+// Dataref:
+// 1-sim/AP/spdSetting
+// 1-sim/AP/spdButton
+// Commands
+// 1-sim/comm/AP/spdDN
+// 1-sim/comm/AP/spdUP
+
+// F/D
+// Dataref:
+// 1-sim/AP/fd1Switcher
+
+// A/T
+// Dataref:
+// 1-sim/AP/atSwitcher
+
+// Disengage A/P
+// Dataref:
+// 1-sim/AP/desengageLever
+
+// LIGHTS
+// Dataref:
+// 1-sim/lights/runwayL/switch
+// 1-sim/lights/runwayL/switch/anim
+// 1-sim/lights/runwayR/switch
+// 1-sim/lights/runwayR/switch/anim
+// 1-sim/lights/landingR/switch
+// 1-sim/lights/landingR/switch/anim
+// 1-sim/lights/landingL/switch
+// 1-sim/lights/landingL/switch/anim
+// 1-sim/lights/landingN/switch
+// 1-sim/lights/landingN/switch/anim
+// Position
+// anim/43/button
+// anim/43/button/anim
+// Red
+// anim/44/button
+// anim/44/button/anim
+// white
+// anim/45/button
+// anim/45/button/anim
+// anim/46/button
+// anim/46/button/anim
